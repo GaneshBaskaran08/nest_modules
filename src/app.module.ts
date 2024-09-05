6aabb980480f2d8d08a502a803a5ec64316dd861
Ganesh <meetganeshbaskar@gmail.com>
2024-09-05 17:25:32 +0530

    - logger middleware added in root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('/');
+    // .forRoutes({ path: 'users/name', method: RequestMethod.GET }); used to specify the path and method
+  }
+}
