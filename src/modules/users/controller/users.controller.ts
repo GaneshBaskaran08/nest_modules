7f94293ffaddf4190ce4f23258951851edc002e9
Ganesh <meetganeshbaskar@gmail.com>
2024-09-06 11:48:40 +0530

    - interceptor added with controller,method and globel level

--- src/modules/users/controller/users.controller.ts
+++ src/modules/users/controller/users.controller.ts
@@ -9,23 +9,26 @@ import {
   Post,
   Query,
   UseGuards,
+  UseInterceptors,
   ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from '../provider/users.service';
 import { CreateUserDto } from '../dto/createUsers.dto';
 import { UpdateUsersDto } from '../dto/updateUsers.dto';
 import { AuthGuard } from 'src/guards/auth.guards';
+import { LoggingInterceptor } from 'src/Interceptors/logging.interceptor';
 
 export interface userDetails {
   id: number;
   name: string;
 }
-
+// @UseInterceptors(LoggingInterceptor) // full rout interceptor // Controller-Level
 @Controller('users')
 @UseGuards(AuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseInterceptors(LoggingInterceptor) // we can set in three way in contoller top full rout or seprate rout or in globale rout // Method-Level
   @Get()
   findAll(): string[] {
     return this.usersService.findAll();
